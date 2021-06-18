27c10f1278c7b99409d99a78e2741d618bf0ffd9
Shubham <sdprajapati3009@gmail.com>
2021-06-18 15:44:50 +0530

    Project is ready but css panding

--- script.js
+++ script.js
@@ -16,11 +16,48 @@ const gamePlay = () => {
     addBtn.innerText = "Done";
 }
 
+const gameEnd = () =>{
+    let date = new Date();
+    endTime = date.getTime();
+    const totalTime = ((endTime - startTime)/1000);
+    
+    let fast = "your time "+" " +totalTime + " seconds ";
 
+    let totalStr = addTxt.value;
+    let wordcount = wordCounter(totalStr);
+    fast += compareText(type.innerText, totalStr);
+    type.innerText = fast;
+}
+const compareText = (str1, str2) => {
+    let a1 = str1.split(" ");
+    let a2 = str2.split(" ");
+    let count = 0;
+
+    a1.forEach(function(item, index){
+        if(item == a2[index]){
+            count++
+        }
+        
+    });
+
+    let errorCount = ( a1.length - count );
+    return (count + " correct word, " +" error words are " +errorCount);
+
+} 
+
+const wordCounter = (str) => {
+    let words = str.split(" ").length;
+    console.log(words);
+    return words;
+}
 
 addBtn.addEventListener('click', function(){
     if(this.innerText == "Start"){
         addTxt.disabled = false;
         gamePlay();
+    }else if(this.innerText == "Done"){
+        addTxt.disabled = true;
+        addBtn.innerText = "Start";
+        gameEnd();
     }
 })
